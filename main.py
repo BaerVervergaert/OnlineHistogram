ebe9bb44501348f938738267cc59524e39b00685
BaerVervergaert <bgmj.ververgaert@gmail.com>
2022-10-26 08:56:01 +0200

    BaseAlgorithm commit

--- main.py
+++ main.py
@@ -3,24 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import bisect
 
-class StatisticalRecord:
-    def __init__(self,name):
-        self.name = name
-
-class HistogramRecord(StatisticalRecord):
-    def __init__(self,name):
-        super(HistogramRecord,self).__init__(self,name)
-        self.bins = np.array([-np.inf, np.inf])
-        self.count = {}
-        self.total_count = 0
-    def add_point(self,x):
-        self.increment_bins(x)
-        self.increment_count(x)
-    def increment_bins(self,x):
-        pass
-    def increment_count(self,x):
-        pass
-
-class OnlineHistogram:
+class BaseOnlineHistogramAlgorithm:
     def __init__(self):
-        self.records = dict()
+        pass
+    def loop(self,stream):
+        for x in stream:
+            self.single_step(x)
+    def single_step(self,x):
+        x = x
+        state = self.find_state(x)
+        if self.constraint(state,x):
+            self.split(state,x)
+        else:
+            self.update(state,x)
+    def find_state(self,x):
+        raise NotImplementedError
+    def constraint(self,state,x):
+        raise NotImplementedError
+    def split(self,state,x):
+        raise NotImplementedError
+    def update(self,state,x):
+        raise NotImplementedError
