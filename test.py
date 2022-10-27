8cf473fc5b1565e7f27a28ae7d56bac347dcf8f3
BaerVervergaert <bgmj.ververgaert@gmail.com>
2022-10-27 22:39:55 +0200

    Test with different distribution and bin limit.

--- test.py
+++ test.py
@@ -11,16 +11,16 @@ def create_test_data(N,d):
 def create_test_data(N,d):
     x = np.zeros(N)
     for i in range(N):
-        if np.random.random()<=.1:
+        if np.random.random()<=.5:
             x[i] = np.random.normal(0,1)
         else:
-            x[i] = np.random.random()-.5
+            x[i] = np.random.normal(10,3)
     data = x
     return(data)
 
 if __name__=='__main__':
     N, d = 10**5, 3
     data = create_test_data(N, d)
-    algo = BoundOnlineHistogramAlgorithm(100)
+    algo = BoundOnlineHistogramAlgorithm(1000)
     algo.loop(data)
-    plot_onedimensional_algo(-1.,1.,algo)
+    plot_onedimensional_algo(-3.,13.,algo)
